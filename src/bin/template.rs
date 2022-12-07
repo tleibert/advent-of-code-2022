fd45a350be65cdb63378d4335e00096e1bcd63da
Trevor Leibert <tleibert1999@gmail.com>
2022-12-07 15:24:53 -0500

    Add template for executables

---
+++ src/bin/template.rs
@@ -0,0 +1,20 @@
+use advent_of_code_2022::{read_file_and_get_input, Problem};
+
+fn main() {
+    let (problem, contents) = read_file_and_get_input();
+
+    let answer = match problem {
+        Problem::One => problem_1(&contents),
+        Problem::Two => problem_2(&contents),
+    };
+
+    println!("Answer: {answer}");
+}
+
+fn problem_1(contents: &str) -> i32 {
+    0
+}
+
+fn problem_2(contents: &str) -> i32 {
+    0
+}
